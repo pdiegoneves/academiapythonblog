b4b496f41ade26240dfebf1f063aaa619aa55bc8
Diego Neves <pdcassiano@gmail.com>
2023-06-06 10:09:08 -0300

    ckeditor

--- blog/urls.py
+++ blog/urls.py
@@ -7,5 +7,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', views.home, name='home'),
     path('posts/<int:post_id>/post_detail', views.post_detail, name='post_detail'),
-    path('ckeditor', include('ckeditor_uploader.urls'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
